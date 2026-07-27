904aa1ff3abadadc9c70bf53be27ba2fcfba136b
Anuar Medina <aimedinai0709@gmail.com>
2026-07-27 13:09:19 -0500

    training

--- QNN/qnn.py
+++ QNN/qnn.py
@@ -71,3 +71,47 @@ def forward(x, w): # first 3 inputs and weight values
     bound_qc = qc.bind_parameters(bind_dict) # Parameters with real numbers
     result = estimator.run(bound_qc, observable).result() # Runs estimator on circuit and observable
     return result.values[0] # Returns scalar output of QNN for that sample
+
+# Training loss with MAE, error on training data
+def loss_train(w):
+    preds = np.array([forward(X_train[i], w) for i in range(len(X_train))]) # predictions
+    return np.mean(np.abs(preds - Y_train))
+
+# Validation loss with MAE, generalization to nontrained data
+def loss_val(w):
+    preds = np.array([forward(X_val[i], w) for i in range(len(X_val))]) # predictions
+    return np.mean(np.abs(preds - Y_val))
+
+# Gradient estimation
+def grad_batch(w, Xb, Yb, epsilon = 0.001): # batch of data
+    g = np.zeros_like(w) # empty vector
+    for k in range(len(w)):
+        w_plus = w.copy(); w_plus[k] += epsilon # shifts up slightly
+        w_minus = w.copy(); w_minus[k] -= epsilon # shifts down slightly
+        preds_plus = np.array([forward(Xb[i], w_plus) for i in range(len(Xb))]) # prediction with slight upward shift
+        preds_minus = np.array([forward(Xb[i], w_minus) for i in range(len(Xb))]) # prediction with slight downward shift
+        g[k] = (np.mean(np.abs(preds_plus - Yb)) - np.mean(np.abs(preds_minus - Yb))) / (2 * epsilon) # gradient of loss to weight
+    return g # tells how to update weights
+
+# Training setup (from paper)
+learning_rate = 0.1 # how large each update to weight is
+batch_size = 10 # number of data samples per batch (days)
+validation_split = 0.1 # what amount of data is used for validation
+epochs = 30 # number of times repeating training, updating epoch
+
+split_index = int(len(X) * (1 - validation_split)) # math to determine which of the dataset is used for training and which is used for validation (first 90% for training and last 10% for validation)
+X_train, Y_train = X[:split_index], Y[:split_index] # training inputs and targets
+X_val, Y_val = X[split_index:], Y[split_index:] # validation inputs and targets
+
+w = np.random.uniform(-0.1, 0.1, size = len(weight_params)) # initial guess for rotations
+
+def get_batch():
+    index = np.random.choice(len(X_train), batch_size, replace = False) # randomly chooses batch and cannot select same batch
+    return X_train[index], Y_train[index] 
+
+for epoch in range(epochs):
+    Xb, Yb = get_batch() # gets random training data
+    g = grad_batch(w, Xb, Yb) # finds gradient of batch loss to weight
+    w -= learning_rate * g # updates weights, if g is negative it will increase and if g is positive it will decrease
+
+    print(f"Epoch {epoch}, train loss = {loss_train(w):.6f}, validation loss = {loss_val(w):.6f}") # finds training and validation loss and prints both to 6 decimals
